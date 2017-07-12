18291e4a92f19ea368fb38ab053ea07953b1d15e
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2017-07-12 20:32:28 -0300

    Remove garbage from globo.com

---
+++ globo-remove.js
@@ -0,0 +1,10 @@
+// Bookmarklet URL:
+// javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/globo-remove.js';})();
+
+(function() {
+  $('.top-header').remove();
+  $('#relacionadas-topo').remove();
+  $('#glb-topo').remove();
+  $('#header').remove();
+  $('.content-ads').remove();
+})();
