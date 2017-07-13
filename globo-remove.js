de3a82e2236593842b510516cc0907405af433f8
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2017-07-13 03:39:45 -0300

    Globo remal improvements

--- globo-remove.js
+++ globo-remove.js
@@ -2,13 +2,20 @@
 // javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/globo-remove.js';})();
 
 (function() {
-  $('.top-header').remove();
-  $('#relacionadas-topo').remove();
+  $('#banner_slb_meio').remove();
+  $('#banner_slb_topo').remove();
   $('#glb-topo').remove();
   $('#header').remove();
   $('#header-produto').remove();
+  $('#oglobo-shopping').remove();
+  $('#onesignal-popover-container').remove();
+  $('#relacionadas-topo').remove();
+  $('#sidebar').remove();
+  $('.box-vejaTambem').remove();
   $('.content-ads').remove();
-  $('.tudosobre-container').remove();
-  $('#banner_slb_meio').remove();
   $('.publicidade').remove();
+  $('.publicidade-materia').remove();
+  $('.teads-inread').remove();
+  $('.top-header').remove();
+  $('.tudosobre-container').remove();
 })();
