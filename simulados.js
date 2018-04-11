c65ee61072ca6f4282f457f3471a3d222ba344d4
Eduardo Antunes C. de Sousa <ecsousa@gmail.com>
2018-04-10 22:59:41 -0300

    Shortcuts for Simulados para Concursos

---
+++ simulados.js
@@ -0,0 +1,23 @@
+// Bookmarklet URL:
+// javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/simulados.js';})();
+
+(function() {
+  $(document).keydown(function(e) {
+    if(!e.altKey) return;
+      
+    if(e.keyCode == 'E'.charCodeAt(0)) {
+      $('.editar_questao')[0].click()
+      event.preventDefault();
+    }
+
+    if(e.keyCode == 'M'.charCodeAt(0)) {
+      $("[data-id='id_materia']").click()
+      event.preventDefault();
+    }
+
+    if(e.keyCode == 'O'.charCodeAt(0)) {
+      $("[id='action_questao']").click()
+      event.preventDefault();
+    }
+  })
+})();
