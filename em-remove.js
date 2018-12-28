f8285ded270cb2f22978bddef991a6336d5bebbf
Eduardo Sousa <ecsousa@gmail.com>
2018-12-28 13:28:30 -0200

    Script to clean up em.com.br

---
+++ em-remove.js
@@ -0,0 +1,27 @@
+// Bookmarklet URL:
+// javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/em-remove.js';})();
+
+(function() {
+  var selectorsToRemove = [
+    'header.header-fixed',
+    'nav.nav-fix',
+    'div.onesignal-bell-launcher-button',
+    'nav.fixed-btn-uai',
+    'header.header-border-underline',
+    'div div.read-more-widget',
+    'div.ads',
+    'div.em-publicidade-retangulo-interna',
+    'section.read-more-about',
+    'aside',
+    'div#taboola-below-article-thumbnails'
+  ];
+
+  selectorsToRemove.forEach(function(item) {
+    var items = $(item);
+    if(items)
+      items.remove();
+    else
+      console.log('Couldn\'t find items for ' + item);
+  });
+
+})();
