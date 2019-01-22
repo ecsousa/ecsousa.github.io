59833bd80e8bc50f01273537353325e02576c0e4
Eduardo Sousa <ecsousa@gmail.com>
2019-01-22 12:32:55 -0200

    Force loading jQuery

--- em-remove.js
+++ em-remove.js
@@ -1,27 +1,50 @@
 // Bookmarklet URL:
-// javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js';document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/em-remove.js';})();
+// javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/em-remove.js';})();
 
-(function() {
-  var selectorsToRemove = [
-    'header.header-fixed',
-    'nav.nav-fix',
-    'div.onesignal-bell-launcher-button',
-    'nav.fixed-btn-uai',
-    'header.header-border-underline',
-    'div div.read-more-widget',
-    'div.ads',
-    'div.em-publicidade-retangulo-interna',
-    'section.read-more-about',
-    'aside',
-    'div#taboola-below-article-thumbnails'
-  ];
+(function(){
 
-  selectorsToRemove.forEach(function(item) {
-    var items = $(item);
-    if(items)
-      items.remove();
-    else
-      console.log('Couldn\'t find items for ' + item);
-  });
+    // the minimum version of jQuery we want
+    var v = "1.3.2";
+
+    // check prior inclusion and version
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+        var done = false;
+        var script = document.createElement("script");
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function(){
+            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                done = true;
+                initMyBookmarklet();
+            }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+        initMyBookmarklet();
+    }
+
+    function initMyBookmarklet() {
+      var selectorsToRemove = [
+        'header.header-fixed',
+        'nav.nav-fix',
+        'div.onesignal-bell-launcher-button',
+        'nav.fixed-btn-uai',
+        'header.header-border-underline',
+        'div div.read-more-widget',
+        'div.ads',
+        'div.em-publicidade-retangulo-interna',
+        'section.read-more-about',
+        'aside',
+        'div#taboola-below-article-thumbnails'
+      ];
+
+      selectorsToRemove.forEach(function(item) {
+        var items = $(item);
+        if(items)
+          items.remove();
+        else
+          console.log('Couldn\'t find items for ' + item);
+      });
+
+    }
 
 })();
--- globo-remove.js
+++ globo-remove.js
@@ -1,60 +1,86 @@
 // Bookmarklet URL:
 // javascript:(function(){document.body.appendChild(document.createElement('script')).src='https://ecsousa.github.io/globo-remove.js';})();
 
-(function() {
-  var selectorsToRemove = [
-    '#banner_slb_meio',
-    '#banner_slb_topo',
-    '#header-cbn',
-    '#header-produto',
-    '#login-barreira',
-    '#oglobo-shopping',
-    '#onesignal-popover-container',
-    '#relacionadas-topo',
-    '#sidebar',
-    '#outbrain-recomendacao2',
-    '#footerOgloboPianoId',
-    '.aep-ads',
-    '.banner-topo',
-    '.barra-assine',
-    '.box-vejaTambem',
-    '.content-ads',
-    '.home-widgets-inferiores',
-    '.hui-publicidade--banner_slb_fim',
-    '.hui-publicidade--banner_slb_meio',
-    '.publicidade',
-    '.publicidade-materia',
-    '.teads-inread',
-    '.top-header',
-    '.site-header',
-    '.tudosobre-container',
-    '[data-google-query-id]',
-    '.block--advertising',
-    '.article-related-links'
-  ];
+(function(){
 
-  $('body').css('overflow', 'auto');
-  $('body').css('display', 'contents');
+    // the minimum version of jQuery we want
+    var v = "1.3.2";
 
-  if($(location).attr('href') != 'http://www.globo.com/') {
-    selectorsToRemove.push('#header');
-    selectorsToRemove.push('#glb-topo');
-  }
+    // check prior inclusion and version
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
+        var done = false;
+        var script = document.createElement("script");
+        script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
+        script.onload = script.onreadystatechange = function(){
+            if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+                done = true;
+                initMyBookmarklet();
+            }
+        };
+        document.getElementsByTagName("head")[0].appendChild(script);
+    } else {
+        initMyBookmarklet();
+    }
 
-  var remover;
+    function initMyBookmarklet() {
+        (window.myBookmarklet = function() {
+          var selectorsToRemove = [
+            '#banner_slb_meio',
+            '#banner_slb_topo',
+            '#header-cbn',
+            '#header-produto',
+            '#login-barreira',
+            '#oglobo-shopping',
+            '#onesignal-popover-container',
+            '#relacionadas-topo',
+            '#sidebar',
+            '#outbrain-recomendacao2',
+            '#footerOgloboPianoId',
+            '.aep-ads',
+            '.banner-topo',
+            '.barra-assine',
+            '.box-vejaTambem',
+            '.content-ads',
+            '.home-widgets-inferiores',
+            '.hui-publicidade--banner_slb_fim',
+            '.hui-publicidade--banner_slb_meio',
+            '.publicidade',
+            '.publicidade-materia',
+            '.teads-inread',
+            '.top-header',
+            '.site-header',
+            '.tudosobre-container',
+            '[data-google-query-id]',
+            '.block--advertising',
+            '.article-related-links'
+          ];
 
-  if($('#frameContent').length > 0) {
-    var contents = $('#frameContent').contents();
-    remover = function(index, item) {
-      contents.find(item).remove();
-    };
-  }
-  else {
-    remover = function(index, item) {
-      $(item).remove();
-    };
-  }
+          $('body').css('overflow', 'auto');
+          $('body').css('display', 'contents');
 
-  $.each(selectorsToRemove, remover);
+          if($(location).attr('href') != 'http://www.globo.com/') {
+            selectorsToRemove.push('#header');
+            selectorsToRemove.push('#glb-topo');
+          }
+
+          var remover;
+
+          if($('#frameContent').length > 0) {
+            var contents = $('#frameContent').contents();
+            remover = function(index, item) {
+              contents.find(item).remove();
+            };
+          }
+          else {
+            remover = function(index, item) {
+              $(item).remove();
+            };
+          }
+
+          $.each(selectorsToRemove, remover);
+
+        })();
+    }
 
 })();
+
