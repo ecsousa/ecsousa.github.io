ecsousa/bookmarklets

Handle case of going outside boundaries

kimble.js
@@ -21,8 +21,13 @@
 
       var nextField = $("tr#" + rowKey + " input[entry-date='" + nextDate + "']");
 
-      nextField.focus().focusin();
-      window.setTimeout(function() { $("input.start-time[row-key='" + rowKey + "'][entry-date='" + nextDate + "']").focus().focusin(); }, 100)
+      if(nextField.length == 0) {
+        $("input.start-time[row-key='" + rowKey + "'][entry-date='" + current + "']").focus().focusin();
+      }
+      else {
+        nextField.focus().focusin();
+        window.setTimeout(function() { $("input.start-time[row-key='" + rowKey + "'][entry-date='" + nextDate + "']").focus().focusin(); }, 100)
+      }
     }
   })
 })();

ecsousa/ecsousa.github.io

kimble.js
@@ -21,8 +21,13 @@
 
       var nextField = $("tr#" + rowKey + " input[entry-date='" + nextDate + "']");
 
-      nextField.focus().focusin();
-      window.setTimeout(function() { $("input.start-time[row-key='" + rowKey + "'][entry-date='" + nextDate + "']").focus().focusin(); }, 100)
+      if(nextField.length == 0) {
+        $("input.start-time[row-key='" + rowKey + "'][entry-date='" + currentDate + "']").focus().focusin();
+      }
+      else {
+        nextField.focus().focusin();
+        window.setTimeout(function() { $("input.start-time[row-key='" + rowKey + "'][entry-date='" + nextDate + "']").focus().focusin(); }, 100)
+      }
     }
   })
 })();